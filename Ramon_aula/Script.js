7d4b5de901920485c495da14d75ded9b004899de
Linnnca <diegolincris638@gmail.com>
2024-07-18 14:08:15 -0300

    Add files via upload

---
+++ Ramon_aula/Script.js
@@ -0,0 +1,16 @@
+var aluno = {
+    nome:'Diego lindo',
+    nota: 10,
+    turma: 103,
+    endereco:{
+        rua:'Palhoça',
+        numero:'39'
+    }
+    
+}
+console.log(aluno.nota)
+//(aluno pega toda a informação do objeto) ja com um ponto pega somente Oq esta dentro da variavel
+
+
+
+
